f15c2bf15b5daeeb0ea94964da9cd1ec19b9d5a0
unknown <federicopalacioush@hotmail.comgit config --global user.name Fedepalaushgit commit -m primera>
2023-05-31 22:25:41 -0300

    Agregado Cruce Dorado

--- pages/CompletoBM.py
+++ pages/CompletoBM.py
@@ -12,6 +12,26 @@ import plotly.graph_objects as go
 
 tickers = ['HL', 'SONY', 'ETSY', 'BABA', 'SID', 'UL', 'KEP', 'LMT', 'MELI', 'MSFT', 'CX', 'AMX', 'HOG', 'PBI', 'TSM', 'MA', 'VALE', 'MMM', 'BCS', 'BBD', 'DESP', 'WBAD', 'TWTR', 'XD', 'RTX', 'CAAP', 'VZD', 'BAD', 'NEM', 'ABEV', 'BIIB', 'HAL', 'ERIC', 'VIV', 'AAPL', 'IP', 'GOOGL', 'HMY', 'AVY', 'SBS', 'BP', 'NGG', 'ITUB', 'TD', 'JNJ', 'ING', 'XOM', 'HSBC', 'UNP', 'AAPLD', 'PAAS', 'TIMB', 'PEP', 'CL', 'NVS', 'MOD', 'BHP', 'HMC', 'PSX', 'HON', 'V', 'AUY', 'BIDU', 'MCD', 'USB', 'IFF', 'SNA', 'MDT', 'AEG', 'PCAR', 'JD', 'TSLA', 'GPRK', 'ANF', 'SLB', 'PG', 'EBAY', 'TM', 'SQ', 'IBM', 'CRMD', 'FSLR', 'FDX', 'GE', 'AZN', 'VOD', 'DEO', 'AEM', 'BB', 'ARCO', 'ADI', 'GSK', 'T', 'SNOW', 'ERJ', 'CAT', 'GILD', 'NMR', 'BIOX', 'KGC', 'GRMN', 'WFCD', 'VRSN', 'JPM', 'GGB', 'DE', 'CRM', 'WBA', 'ADBE', 'ZM', 'SAN', 'TMO', 'AMD', 'GOLD', 'BMY', 'BA', 'NFLX', 'MO', 'NTES', 'KB', 'QCOM', 'MRK', 'X', 'PGD', 'EFX', 'MUFG', 'AVGO', 'COST', 'MFG', 'WBK', 'UGP', 'ROST', 'VZ', 'ADGO', 'XROX', 'DOCU', 'XOMD', 'AXP', 'NKE', 'YELP', 'FCX', 'PFE', 'AMZN', 'INTC', 'CS', 'WMTD', 'GS', 'C', 'HD', 'MSI', 'KOD', 'CDE', 'LYG', 'RIO', 'SCCO', 'E', 'DD', 'SBUX', 'CVX', 'IBN', 'ORAN', 'CAR', 'KO', 'YY', 'WFC', 'AMAT', 'PBR', 'SAP', 'HWM', 'ABBV', 'SHOP', 'INFY', 'BRFS', 'LVS', 'TTM', 'GFI', 'FMX', 'VISTD', 'URBN', 'AIG', 'BK', 'HPQ', 'AMGN', 'NUE', 'VD', 'GLW', 'KMB', 'EBR', 'TRIP', 'GSD', 'SUZ', 'TXN', 'PHG', 'NTCO', 'WMT', 'SPOT', 'CSCO', 'WBO', 'HSY', 'ORCL', 'ZMD', 'SNAP', 'LLY', 'MMC', 'CAH', 'UNH', 'ABT', 'BSBR', 'NVDA', 'ADP', 'TGT', 'VIST', 'PYPL']
 
+def graficoVelas(data):
+    #Grafico de Velas
+            st.subheader("Grafico de Velas")
+            # Create a subplot with shared x-axis
+            fig = make_subplots(shared_xaxes=True)
+            # Add the candlestick trace
+            candlestick_trace = go.Candlestick(x=data.index,
+                                            open=data['Open'],
+                                            high=data['High'],
+                                            low=data['Low'],
+                                            close=data['Close'])
+            fig.add_trace(candlestick_trace)
+            # Add range breaks to the x-axis
+            rangebreaks = [
+                dict(bounds=["sat", "mon"]),  # Range break from Saturday to Monday
+            ]
+            fig.update_layout(xaxis_rangeslider_visible=False,  # Disable the rangeslider
+                            xaxis=dict(rangebreaks=rangebreaks))  # Add the range breaks
+            # Display the chart
+            st.plotly_chart(fig)
 
 for ticker in tickers:
     data = yf.Ticker(ticker).history('5y')
@@ -50,29 +70,20 @@ for ticker in tickers:
 
         if data['SALIDACORTO'].tail(3).any():
             if (data['SALIDACORTO'].tail(3) == 1).any():
-                st.write('LONG')
+                st.write('LONG' + 'CORTO')
             if (data['SALIDACORTO'].tail(3) == 2).any():
-                st.write('SHORT')
+                st.write('SHORT' + 'CORTO')
             st.write(ticker)
-        #Grafico de Velas
-            st.subheader("Grafico de Velas")
-            # Create a subplot with shared x-axis
-            fig = make_subplots(shared_xaxes=True)
-            # Add the candlestick trace
-            candlestick_trace = go.Candlestick(x=data.index,
-                                            open=data['Open'],
-                                            high=data['High'],
-                                            low=data['Low'],
-                                            close=data['Close'])
-            fig.add_trace(candlestick_trace)
-            # Add range breaks to the x-axis
-            rangebreaks = [
-                dict(bounds=["sat", "mon"]),  # Range break from Saturday to Monday
-            ]
-            fig.update_layout(xaxis_rangeslider_visible=False,  # Disable the rangeslider
-                            xaxis=dict(rangebreaks=rangebreaks))  # Add the range breaks
-            # Display the chart
-            st.plotly_chart(fig)
+            graficoVelas(data)
+        if data['SALIDALARGO'].tail(3).any():
+            if (data['SALIDALARGO'].tail(3) == 1).any():
+                st.write('LONG' + 'LARGO')
+            if (data['SALIDALARGO'].tail(3) == 2).any():
+                st.write('SHORT' + 'LARGO')
+            st.write(ticker)    
+            graficoVelas(data)
+
+        
     
     except:
         pass
