94ee13610bf6a61d2f9fcfb182110ffcbe2dff21
unknown <federicopalacioush@hotmail.comgit config --global user.name Fedepalaushgit commit -m primera>
2023-06-04 21:09:56 -0300

    data separada

--- pages/CompletoBM.py
+++ pages/CompletoBM.py
@@ -7,10 +7,7 @@ import pandas_ta as ta
 import streamlit as st
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
-
-
-
-tickers = ['HL', 'SONY', 'ETSY', 'BABA', 'SID', 'UL', 'KEP', 'LMT', 'MELI', 'MSFT', 'CX', 'AMX', 'HOG', 'PBI', 'TSM', 'MA', 'VALE', 'MMM', 'BCS', 'BBD', 'DESP', 'WBAD', 'TWTR', 'XD', 'RTX', 'CAAP', 'VZD', 'BAD', 'NEM', 'ABEV', 'BIIB', 'HAL', 'ERIC', 'VIV', 'AAPL', 'IP', 'GOOGL', 'HMY', 'AVY', 'SBS', 'BP', 'NGG', 'ITUB', 'TD', 'JNJ', 'ING', 'XOM', 'HSBC', 'UNP', 'AAPLD', 'PAAS', 'TIMB', 'PEP', 'CL', 'NVS', 'MOD', 'BHP', 'HMC', 'PSX', 'HON', 'V', 'AUY', 'BIDU', 'MCD', 'USB', 'IFF', 'SNA', 'MDT', 'AEG', 'PCAR', 'JD', 'TSLA', 'GPRK', 'ANF', 'SLB', 'PG', 'EBAY', 'TM', 'SQ', 'IBM', 'CRMD', 'FSLR', 'FDX', 'GE', 'AZN', 'VOD', 'DEO', 'AEM', 'BB', 'ARCO', 'ADI', 'GSK', 'T', 'SNOW', 'ERJ', 'CAT', 'GILD', 'NMR', 'BIOX', 'KGC', 'GRMN', 'WFCD', 'VRSN', 'JPM', 'GGB', 'DE', 'CRM', 'WBA', 'ADBE', 'ZM', 'SAN', 'TMO', 'AMD', 'GOLD', 'BMY', 'BA', 'NFLX', 'MO', 'NTES', 'KB', 'QCOM', 'MRK', 'X', 'PGD', 'EFX', 'MUFG', 'AVGO', 'COST', 'MFG', 'WBK', 'UGP', 'ROST', 'VZ', 'ADGO', 'XROX', 'DOCU', 'XOMD', 'AXP', 'NKE', 'YELP', 'FCX', 'PFE', 'AMZN', 'INTC', 'CS', 'WMTD', 'GS', 'C', 'HD', 'MSI', 'KOD', 'CDE', 'LYG', 'RIO', 'SCCO', 'E', 'DD', 'SBUX', 'CVX', 'IBN', 'ORAN', 'CAR', 'KO', 'YY', 'WFC', 'AMAT', 'PBR', 'SAP', 'HWM', 'ABBV', 'SHOP', 'INFY', 'BRFS', 'LVS', 'TTM', 'GFI', 'FMX', 'VISTD', 'URBN', 'AIG', 'BK', 'HPQ', 'AMGN', 'NUE', 'VD', 'GLW', 'KMB', 'EBR', 'TRIP', 'GSD', 'SUZ', 'TXN', 'PHG', 'NTCO', 'WMT', 'SPOT', 'CSCO', 'WBO', 'HSY', 'ORCL', 'ZMD', 'SNAP', 'LLY', 'MMC', 'CAH', 'UNH', 'ABT', 'BSBR', 'NVDA', 'ADP', 'TGT', 'VIST', 'PYPL']
+from dataBM import tickersBM
 
 def graficoVelas(data):
     #Grafico de Velas
@@ -33,6 +30,7 @@ def graficoVelas(data):
             # Display the chart
             st.plotly_chart(fig)
 
+tickers = tickersBM
 for ticker in tickers:
     data = yf.Ticker(ticker).history('5y')
     try:
--- pages/RSI.py
+++ pages/RSI.py
@@ -8,11 +8,12 @@ import seaborn as sns
 import streamlit as st
 import datetime
 from datetime import timedelta
+from dataBM import tickersBM
 
 with st.sidebar:
     add_combo = st.selectbox(
         "Seleccione una Acción",
-        ['AAPL', 'MSFT', 'KO', 'BA'],
+        tickersBM,
         index=0 
 )
 
@@ -21,11 +22,17 @@ with st.sidebar:
         datetime.date.today() - timedelta(days=365))
 
 
+    # Add a slider with two values
+    min_max = st.slider("Seleccione niveles minimos y máximos", 20, 70, (40, 50), 2)
+    ventana = st.slider("Ventana RSI", min_value=7, max_value=21, value=14, step=1)
+
+    
+
 data = yf. download (add_combo, fechaInicio)
 class Rsi(Strategy):
-    upper_bound = 66
-    lower_bound = 38
-    rsi_window = 16
+    upper_bound = min_max[1]
+    lower_bound = min_max[0]
+    rsi_window = ventana
     def init(self):
           self.rsi = self.I(ta.rsi, pd.Series(self.data.Close), self.rsi_window)
     def next(self):
@@ -38,6 +45,7 @@ class Rsi(Strategy):
 
 bt = Backtest(data, Rsi, cash=1000, commission=.002)        
 bt.run()
+
   
 stats, heatmap = bt.optimize(
     upper_bound = range(40,70,2),
@@ -46,9 +54,14 @@ stats, heatmap = bt.optimize(
     maximize= 'Equity Final [$]',
     return_heatmap=True
 )
-
+retorno_final = "{:.2f}%".format(round(stats['Return [%]'], 2))
 heatmap.unstack()
 
-st.text('ARRIBA ' + str(stats["_strategy"].upper_bound))
-st.text('ABAJO ' + str(stats["_strategy"].lower_bound))
-st.text('RSI ' + str(stats["_strategy"].rsi_window))
+st.text('Arriba ' + str(stats["_strategy"].upper_bound))
+st.text('Abajo ' + str(stats["_strategy"].lower_bound))
+st.text('Ventana ' + str(stats["_strategy"].rsi_window))
+st.text('Retorno ' + str(retorno_final))
+
+st.button('Graficar', on_click=bt.plot)
+
+print(stats)
