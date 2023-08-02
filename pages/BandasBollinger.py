ba336f6eaa112aa85d91a130af978806e1f30f39
unknown <federicopalacioush@hotmail.comgit config --global user.name Fedepalaushgit commit -m primera>
2023-08-02 10:45:23 -0300

    bb

--- pages/BandasBollinger.py
+++ pages/BandasBollinger.py
@@ -16,16 +16,12 @@ for ticker in tickersBM:
 
         # Print the DataFrame with Bollinger Bands
         precio_actual = data['Close'].tail(1).values[0]
-        banda_inferior = bollinger_bands.tail(2)['BBL_20_2.0'].values[0]
-        banda_superior = bollinger_bands.tail(2)['BBU_20_2.0'].values[0]
+        banda_inferior = bollinger_bands.tail(3)['BBL_20_2.0'].values
+        banda_superior = bollinger_bands.tail(3)['BBU_20_2.0'].values[0]
 
-        if (precio_actual>banda_superior):
-            print()
-            st.text (ticker)
-            st.text('Venta')
-        elif (precio_actual<banda_inferior):
-            st.text (ticker)
-            st.text("Compra")
+          
+        if ((precio_actual<banda_inferior)).any():
+            st.text(ticker + " Compra")
         else:
             pass        
     except:
